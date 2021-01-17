ad245a322522e7f0a19a6535b0557274b4eae293
Deepak <deepsrh1801@gmail.com>
2021-01-17 22:52:49 +0530

    design implementation in forgetpassword page

--- src/Login/ForgetPassword/ForgetPassword.js
+++ src/Login/ForgetPassword/ForgetPassword.js
@@ -1,11 +1,23 @@
 import React from "react";
 import Header from "../../Header/Header";
-import "./ForgetPassword.css"
+import "./ForgetPassword.css";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import ForgetPasswordimg from "../../assests/Frame.png";
 
 function ForgetPassword() {
   return (
     <div className="forgetPassword">
       <Header />
+      <div className="forgetPassword__content">
+        <ArrowBackIcon className="forgetPassword__arrowBackIcon" />
+        <h2>Forget Password ?</h2>
+      </div>
+
+      <img src={ForgetPasswordimg} className="forgetPassword__img" />
+      <h3>Please enter the Email associated whit your account</h3>
+      <input type="text" />
+      <p>You'll recieve a 4-digit code in your email from verification</p>
+      <button>Send</button>
     </div>
   );
 }
